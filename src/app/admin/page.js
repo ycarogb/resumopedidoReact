b9692b3ff10176ba93eda15d77b3a3dd339b5525
Ycaro Batalha <ycarobatalha1@gmail.com>
2025-11-14 17:37:23 -0300

    implementa componentes da tela principal e cria página para cadastro de produtos

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -1,9 +1,64 @@
-const AdminPage = () => {
+"use client";
+
+import { useRouter } from "next/navigation";
+import LoggoutButton from "../components/LoggoutButton";
+import { useEffect } from "react";
+import Button from "../components/Button";
+
+export default function AdminLayout({ children }) {
+  const router = useRouter();
+
+  const handleCadastrarProduto = () => {
+    router.push("/cadastrar-produto");
+  };
+
+  useEffect(() => {
+    const dadosAutenticacao = JSON.parse(localStorage.getItem("auth"));
+
+    if (!dadosAutenticacao?.isLoggedIn) {
+      router.replace("/");
+    }
+  }, [router]);
+
   return (
     <>
-      <h1> Página principal da aplicação</h1>
+      <div style={{ position: "fixed", top: 20, right: 20 }}>
+        <LoggoutButton />
+      </div>
+      <div
+        style={{
+          maxWidth: 300,
+          margin: "50px auto",
+          padding: 20,
+          border: "1px solid #ccc",
+          borderRadius: 8,
+        }}
+      >
+        <Button
+          style={{
+            maxWidth: 500,
+            width: "100%",
+            margin: "10px auto",
+            padding: 20,
+            border: "1px solid #ccc",
+            borderRadius: 8,
+          }}
+          label={"Cadastrar produto"}
+          onclick={handleCadastrarProduto}
+        />
+        <Button
+          style={{
+            maxWidth: 500,
+            width: "100%",
+            margin: "10px auto",
+            padding: 20,
+            border: "1px solid #ccc",
+            borderRadius: 8,
+          }}
+          label={"Novo Pedido"}
+          onclick={handleCadastrarProduto}
+        />
+      </div>
     </>
   );
-};
-
-export default AdminPage;
+}
